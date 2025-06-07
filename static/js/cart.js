bcd50f3b669a010c46bb61a778cf14783e677db0
ADCY245 <athulnair3096@gmail.com>
2025-06-07 12:42:59 +0530

    Update cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,3 +1,25 @@
+function addToCart(product) {
+  fetch('/add_to_cart', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(product)
+  })
+  .then(res => res.json())
+  .then(data => {
+    const msgDiv = document.getElementById("cart-message");
+    if (data.success) {
+      msgDiv.innerHTML = `<div class="alert alert-success">${data.message}</div>`;
+    } else {
+      msgDiv.innerHTML = `<div class="alert alert-danger">${data.message}</div>`;
+    }
+  })
+  .catch(err => {
+    document.getElementById("cart-message").innerHTML =
+      `<div class="alert alert-danger">Something went wrong.</div>`;
+  });
+}
 function removeFromCart(productId) {
   fetch('/remove_from_cart', {
     method: 'POST',
